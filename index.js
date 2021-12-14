3dc03ca3938af268f19b88a2809ae2a594a46259
princegobah <babynamek@gmail.com>
2021-12-14 16:04:21 -0500

    commit tree 8121b951a6d5bc14e592cade11c1ceb482a8fe0f

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){return (Math.abs(block-42))}
+//console.log(distanceFromHqInBlocks(50))
+function distanceFromHqInFeet(block){return (Math.abs(block-42)*264)}
+console.log(distanceFromHqInFeet(50))
+function distanceTravelledInFeet(block1,block2){return (Math.abs(block1-block2)*264)}
+
+function calculatesFarePrice(start,destination){if (Math.abs(start-destination)*256<=400)return 0
+else if (Math.abs(start-destination)*264>400 && Math.abs(start-destination)*256<2000) return (Math.abs(start-destination)*1.28)
+else if (Math.abs(start-destination)*264>2000 &&Math.abs(start-destination)*256<2500)return 25
+else if (Math.abs(start-destination)*264>2500)return 'cannot travel that far'}
+//console.log(calculatesFarePrice(34,32))
